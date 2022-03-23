6b36d7c878c832f48d2ea7d3e46ebd6a679c3a09
YasserAlvarez <yasser.alvarez.is@unipolidgo.edu.mx>
2022-03-23 00:04:00 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
-	port: 3306,
-	database: "adoption_system",
-	secret_key : "15fldsmdr15" 
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "b134f4a3d03ac4",
+    password: "afb419ba",
+    port: 3306,
+    database: "heroku_40ff34ee5194653",
+    secret_key: "15fldsmdr15"
 };
 exports.config = config;
