3559e2e88931d4f7af7ad664c242894cf86894c8
YasserAlvarez <yasser.alvarez.is@unipolidgo.edu.mx>
2022-03-23 00:13:03 -0600

    Update jwtValidation.js

--- jwtValidation.js
+++ jwtValidation.js
@@ -1,32 +1,30 @@
 const jwt = require("jsonwebtoken");
 const db = require("./database").config;
 const middleware = {
-    validateToken : function(req,res,next){
+    validateToken: function(req, res, next) {
         const bearerHeader = req.headers["authorization"];
-        if(typeof bearerHeader !== 'undefined'){
+        if (typeof bearerHeader !== 'undefined') {
             const bearer = bearerHeader.split(" ");
-            const bearerToken = bearer[1];
+            const bearerToken = bearer[5];
             jwt.verify(bearerToken, db.secret_key, (err, data) => {
-                if(err){
+                if (err) {
                     res.status(401);
                     res.json({
-                        "success" : false,
-                        "code" : 401,
-                        "message" : "Invalid token",
-                        "data" : err
+                        "success": false,
+                        "code": 401,
+                        "message": "Invalid token",
+                        "data": err
                     });
-                }
-                else{
+                } else {
                     next()
                 }
             });
-        }
-        else{
+        } else {
             res.json({
-                "success" : false,
-                "code" : 401,
-                "message" : "You need a token",
-                "data" : []
+                "success": false,
+                "code": 401,
+                "message": "You need a token",
+                "data": []
             });
         }
     }
